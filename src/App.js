3d552a4e35b4554d11e868189a723574abe2662f
DoubleTri <doubletriangle880@gmail.com>
2018-10-17 10:15:26 -0400

    Put button and text in header

--- src/App.js
+++ src/App.js
@@ -17,15 +17,14 @@ class App extends Component {
     <header className="App-header">
      <img src={logo} className="App-logo" alt="logo" />
      <h1 className="App-title">Welcome to React</h1>
+      <p className="App-intro">
+        <> 
+          {JSON.stringify(this.props)} 
+        </>
+        <button onClick={this.simpleAction}>Test redux action</button>
+        <p>To get started, edit <code>src/App.js</code> and save to reload</p>
+      </p>
     </header>
-    <p className="App-intro">
-    <> {
-        JSON.stringify(this.props)
-      } 
-    </>
-    <button onClick={this.simpleAction}>Test redux action</button>
-     To get started, edit <code>src/App.js</code> and save to reload
-    </p>
    </div>
   );
  }
