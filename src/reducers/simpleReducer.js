da597f45c1bd39af915f887dba3d644a66cc0d85
DoubleTri <doubletriangle880@gmail.com>
2018-10-19 10:43:07 -0400

    Removed Firebase from initial state

--- src/reducers/simpleReducer.js
+++ src/reducers/simpleReducer.js
@@ -1,16 +1,9 @@
 import { db } from "../firebase";
 
 
-export default (state = 
-  db.once("value").then(function(snapshot) {
-    return snapshot.val();
-  }, function(error) {
-    console.error(error);
-  })  
-  , action) => {
+export default (state = {}, action) => {
     switch (action.type) {
      case 'SIMPLE_ACTION':
-     console.log(state)
       return {
        result: action.payload.result,
        pastColor: action.payload.pastColor   
